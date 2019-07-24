61dd84e1f9000848da69a91614905d6773e8459c
RahmadTriadi <52515306+RahmadTriadi@users.noreply.github.com>
2019-07-24 14:22:09 +0700

    Membuat tampilan awal sebelum login

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Awal</title>
+</head>
+<body>
+<h3>Selamat Datang Di Perpustakaan Online</h3>
+    <p>Pada Sistem Ini dapat membaca beberapa pilihan buku yang dapat dilihat secara online</p>
+    <a href="login.php" /><button class="btn btn-primary btn-3d btn-login">Sudah memiliki akun?</button></a>
+    <a href="daftar.php" /><button class="btn btn-default btn-login">Membuat akun Baru</button></a>
+</body>
+</html>
