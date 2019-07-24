7111c150e5f5078ee4afa3989bf03db678a177d5
RahmadTriadi <52515306+RahmadTriadi@users.noreply.github.com>
2019-07-24 14:23:26 +0700

    Update index.php

--- Member/index.php
+++ Member/index.php
@@ -7,7 +7,7 @@
 <body>
   <ul>
     <li><a href="index.php">Beranda</a></li>
-    <li>a href="buku.php">Tabel Buku</a></li>
+    <li><a href="buku.php">Tabel Buku</a></li>
   </ul>
   <h3 class="animated fadeInLeft">Home/Beranda</h3>
   <p class="animated fadeInDown">Selamat Datang Member di Perpustakaan Online</p>
