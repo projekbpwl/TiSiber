4c048d6920c29bb303320e6758877f7540021874
RahmadTriadi <52515306+RahmadTriadi@users.noreply.github.com>
2019-07-24 14:19:40 +0700

    membuat tampilan home pada member

---
+++ Member/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My Project</title>
+</head>
+
+<body>
+  <ul>
+    <li><a href="index.php">Beranda</a></li>
+    <li>a href="buku.php">Tabel Buku</a></li>
+  </ul>
+  <h3 class="animated fadeInLeft">Home/Beranda</h3>
+  <p class="animated fadeInDown">Selamat Datang Member di Perpustakaan Online</p>
+  <a href="../logout.php">Logout</a>
+</body>
+</html>
