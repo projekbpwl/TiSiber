20206c266854de0bfa0ea367cdf82f6dab5da064
RahmadTriadi <52515306+RahmadTriadi@users.noreply.github.com>
2019-07-24 14:17:19 +0700

    Membuat home pada admin

---
+++ Admin/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Perpustakaan Online</title>
+</head>
+
+<body>
+	<ul>
+    <li><a href="index.php">Beranda</a></li>
+    <li><a href="mahasiswa.php">Data Mahasiswa</a></li>
+    <li><label>Data Buku</label>
+      <ul>
+        <li><a href="buku.php">Tabel Buku</a></li>
+        <li><a href="tambahbuku.php">Tambah Buku</a></li>
+      </ul>
+    </li>
+  </ul>
+  <h3 class="animated fadeInLeft">Home/Beranda</h3>
+  <p class="animated fadeInDown">Selamat Datang Admin di Perpustakaan Online</p>
+  <a href="../logout.php">Logout</a>
+</body>
+</html>
